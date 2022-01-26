c7253de14b02f21b0486ec446797f5c96baa8df5
shvetsov-as <a002eshvetsov@gmail.com>
2022-01-26 16:47:29 +0300

    Autowired, Qualified, Factory methods in annotations (constructor, setter, field)

--- src/main/java/ru/shvetsov/spring/MusicPlayer.java
+++ src/main/java/ru/shvetsov/spring/MusicPlayer.java
@@ -1,44 +1,54 @@
 package ru.shvetsov.spring;
 
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 
+
 @NoArgsConstructor
+@Component
 //IoC
 public class MusicPlayer {
-    private List<IMusic> musicList;
+//      field injection
+//      @Autowired
+// may be @Qualifier ("classicalMusic") to avoid duplication if [private IMusic music] is interface and we have several same beans;
+//    private ClassicalMusic classicalMusic;
+//    private RockMusic rockMusic;
+//    private PopMusic popMusic;
 
-    private String name;
-    private int volume;
+//    @Autowired
+//    public MusicPlayer(ClassicalMusic classicalMusic, RockMusic musicBean, PopMusic popMusic) {
+//        this.classicalMusic = classicalMusic;
+//        this.rockMusic = musicBean;
+//        this.popMusic = popMusic;
+//    }
 
-    public MusicPlayer(List<IMusic> musicList) {
-        this.musicList = musicList;
+//    Setter injection
+//    @Autowired
+//    public void setMusicORanotherNAMEallWillBeCorrect(IMusic music) {
+//        this.music = music;
+//    }
+
+    private IMusic music1;
+    private IMusic music2;
+    private IMusic music3;
+
+@Autowired
+    public MusicPlayer(@Qualifier("classicalMusic") IMusic music1,
+                       @Qualifier("musicBean")IMusic music2,
+                       @Qualifier("popMusic")IMusic music3) {
+        this.music1 = music1;
+        this.music2 = music2;
+        this.music3 = music3;
     }
 
-    public void setMusicList(List<IMusic> musicList) {
-        this.musicList = musicList;
+    public String playMusic() {
+        return "Now playing " + music1.getSong();
+//        System.out.println("Now playing " + rockMusic.getSong());
+//        System.out.println("Now playing " + popMusic.getSong());
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getVolume() {
-        return volume;
-    }
-
-    public void setVolume(int volume) {
-        this.volume = volume;
-    }
-
-    public void playMusic() {
-        musicList.stream().forEach(music -> System.out.println(music.getSong()));
-    }
-
-
 }
 
--- src/main/java/ru/shvetsov/spring/TestSpring.java
+++ src/main/java/ru/shvetsov/spring/TestSpring.java
@@ -8,16 +8,16 @@ public class TestSpring {
         //IMusic music = context.getBean("musicBean", IMusic.class);
         //MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        //Setter Dep injection from applicationContext.xml using musicPlayer.properties
+
         //Can change implementation class in applicationContext.xml
         //Init & Destroy - type VOID
         //No input args!
         //For scope = "prototype" -> spring doesn't call Destroy-method
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
 
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
+        Computer computer = context.getBean("computer", Computer.class);
+        System.out.println(computer);
 
         context.close();
     }
